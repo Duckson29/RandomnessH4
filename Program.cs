690f2bcfce22c7678ce7ead7a56d24c82adea3e3
Andi Olle Olsen <zbcanols21@zbc.dk>
2020-10-26 16:39:27 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.Security.Cryptography;
+
+namespace RNGCryptoServiceProviderH4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            int n = 100000000;
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                byte[] data = new byte[4];
+                int value = 0;
+                stopwatch.Start();
+                for (int i = 0; i < n; i++)
+                {
+                    rng.GetBytes(data);
+                    value = BitConverter.ToInt32(data, 0);
+
+                }
+            }
+                stopwatch.Stop();
+                TimeSpan time = stopwatch.Elapsed;
+                Console.WriteLine("RNGCryptoServiceProvider over n(100000) total secs : " + time.TotalSeconds);
+                Random ran = new Random();
+                stopwatch.Reset();
+                stopwatch.Start();
+                for (int i = 0; i < n; i++)
+                {
+                    ran.Next();
+                }
+                stopwatch.Stop();
+                Console.WriteLine("RNGCryptoServiceProvider over n(100000) total secs : " + stopwatch.Elapsed.TotalSeconds);                
+            
+
+        }
+    }
+}
